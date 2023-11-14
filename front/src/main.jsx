e4afe27b7a2e1382f08bd33e44f3eeb6a2a2b4a2
jeremy-to <jeremy.to@ecole2600.com>
2023-11-14 18:08:32 +0100

    add home page

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import App from './pages/App.jsx';
+import SignIn from './pages/signIn/index.jsx';
+import Layout from './pages/layout/Index.jsx';
+import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<App />} />
+					<Route path="/login" element={<SignIn />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
+);
